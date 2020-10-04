f8b8d8a7d56548277ce8eabd80a99b6ac494e50b
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-10-04 11:21:30 +1100

    Add project files.

---
+++ tests/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System;
+using System.IO.Ports;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+namespace config
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            #region Setup
+
+            #endregion
+
+            #region Visibility Tests
+
+            #endregion
+        }
+    }
+}
